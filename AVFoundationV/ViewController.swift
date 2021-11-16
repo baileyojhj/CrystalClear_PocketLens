0a41e7f365d11ae66340872b7c9f0c44df6d5f68
Oliver Bailey <baileyojhj@gmail.com>
2021-11-16 11:17:00 -0500

    Initial Commit

---
+++ AVFoundationV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVFoundationV
+//
+//  Created by Oliver Bailey on 2021-11-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
